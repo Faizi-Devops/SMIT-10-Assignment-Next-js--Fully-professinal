92dd3b4abc5c3797457fbbfae605af04446be87f
Faizan Ali <devfaizi435@gmail.com>
2022-11-06 22:16:55 -0800

    Footer Section

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,52 @@
 const Footer = () =>{
     return(
-        <>
-        <div className="back1">
+        <div className="container-fluid back1 ">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-2 col-md-6 col-sm-12">
+                        <h3 className="text-back A">HYPEINDUSTRY</h3>
+                            <p className="text-back B">HypeIndustry Concept</p>
+                            <p className="text-back B">Buying Guide</p>
+                            <p className="text-back B">Selling Guide</p>
+                    </div>
+                    <div className="col-lg-2 col-md-6 col-sm-12">
+                        <h3 className="text-back A">HYPE OFFERS</h3>
+                            <p className="text-back B">Footwear</p>
+                            <p className="text-back B">Apparel</p>
+                            <p className="text-back B">Accessories</p>
+                    </div>
+
+                    <div className="col-lg-2 col-md-6 col-sm-12 ">
+                        <h3 className="text-back A">SHIPPING</h3>
+                            <p className="text-back B">Shipping For Buyers</p>
+                            <p className="text-back B">Shipping For Sellers</p>
+                            <p className="text-back B">Accessories</p>
+                    </div>
+
+                    <div className="col-lg-2 col-md-0 col-sm-0">
+
+                    </div>
+                    <div className="col-lg-2 col-md-6 col-sm-12">
+                    <h3 className="text-back A">CONTACT US</h3>
+                    <p className="text-col B">fa1694701@gmail.com</p>
+                    <p className="text-col B">MinahilShafique@gmail.com</p>
+
+
+                    </div>
+
+                    
+                    
+
+                    
+
+                </div>
+
+            </div>
+            <br /><br /><br />
+            <p className="text-back B text-center">@Shahzada Faizi</p>
+            <br />
 
         </div>
-
-
-        </>
     )
 }
 export default Footer;
