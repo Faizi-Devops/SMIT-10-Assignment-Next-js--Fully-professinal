0c52eaa66943c4323042f82cb1e7e051c6ee48bc
Faizan Ali <devfaizi435@gmail.com>
2022-11-09 21:55:35 -0800

    Shipping1-Page-Complete

--- pages/Shipping1.tsx
+++ pages/Shipping1.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar/Navbar";
 import Image from 'next/image';
+import Footer from "../components/Footer/Footer";
 
 const Shipping1 = () => {
     return (
@@ -29,9 +30,53 @@ const Shipping1 = () => {
 
                 </div>
 
+                <div className="row Ship1-Second-Div">
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        
+
+                        
+                        <Image className="image-Overflow"
+
+                            src="/4.jpg"
+                            alt="Picture of the author"
+                            width={500}
+                            height={200}
+                            
+                        />
+                       
+
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className="container">
+
+                      
+                        <p className="Ship1-Second-Paragraph">How much does shipping COST</p>
+                        <p className="Ship1-paragraph-New">The default shipping cost for sellers & buyers on the platform is $2.50/pair for domestic shipments within the USA, and $10-$12.50/pair for international express shipments. That will vary based on the seller & buyer location. Of course, shipping cost are pretty low, as we are dealing with bulk purchases, resulting in cost-effective shipping.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                
+
             </div>
+            <div className="Ship1-Third-Div">
+                    <div className="Ship1-under-Third">
+                        <p className="Third-Div-1">Can I change my shipping address once my order has been placed?</p>
+                        <p className="Third-Div-Paragraph">Hypeindustry is unable to change a shipping address once the order has been placed. So please make sure all personal and <br /> corporate information are correct on your user profile.Also the shipping address on file has to match your billing address!</p>
+                        <p className="Third-Div-1">Why does my billing and shipping address have to MATCH?</p>
+                        <p className="Third-Div-1">Can I edit my order once I placed It?</p>
+                        <p className="Third-Div-1">Can I change the Shipping Address AFTER the order has been placed?</p>
+                        <p className="Third-Div-1 Sip">How long does it take to ship?</p>
+                        <br />
+                    </div>
+
+                </div>
+                <Footer />
 
         </div>
+        
     )
 }
 export default Shipping1;
